404c616b988917ff2a172977cb38abc8d81e1ed7
Ian Schwartz <ian.schwartz23@gmail.com>
2020-03-10 14:18:18 -0700

    djb2 stretch

--- src/hashtable.py
+++ src/hashtable.py
@@ -33,7 +33,10 @@ class HashTable:
 
         OPTIONAL STRETCH: Research and implement DJB2
         '''
-        pass
+        hash_num = 5381
+        for c in key:
+            hash_num = hash_num * 33 + ord(c)
+        return hash_num
 
 
     def _hash_mod(self, key):
@@ -41,7 +44,7 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
-        return self._hash(key) % self.capacity
+        return self._hash_djb2(key) % self.capacity
 
 
     def insert(self, key, value):
